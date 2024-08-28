3d982b5a740ea04108b76f9b678df76a30aa7984
ddakker <ddakker@openmaru.io>
2024-08-28 23:58:49 +0900

    first commit

---
+++ src/main/java/io/openmaru/test/springboot3/test_springboot3/TestSpringboot3Application.java
@@ -0,0 +1,13 @@
+package io.openmaru.test.springboot3.test_springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/openmaru/test/springboot3/test_springboot3/TestSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.openmaru.test.springboot3.test_springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
